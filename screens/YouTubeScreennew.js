3e7649fe08315433b1d7c13c376f5b9fef6ba7bd
reaggaebasherswagwanbigtunetin-art <reaggaebasherswagwanbigtunetin@gmail.com>
2025-08-31 19:50:56 +0100

    Create YouTubeScreennew.js with initial imports

---
+++ screens/YouTubeScreennew.js
@@ -0,0 +1,4 @@
+import React, { useState, useRef } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList, StyleSheet } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+import { youtubeSearch, trainAlgo
